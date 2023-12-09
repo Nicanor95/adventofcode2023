935f69494c7487ff525b2ce4ca570941784173d1
Arancibia Leiria Leandro Nicanor <Nicanor95@users.noreply.github.com>
2023-12-09 07:39:15 -0300

    Add Part 2 of Day 4

--- Day 4/cards.py
+++ Day 4/cards.py
@@ -4,6 +4,7 @@ with open(r'./input.txt', mode='r', encoding='utf-8') as inputfile:
 	# It'd be faster to use str.split() but I love regex.
 	regex = re.compile(r'(?:^Card +)(?P<ID>\d+)(?:: +)(?P<winning_numbers>.+)(?: \| +)(?P<numbers_in_card>.+)(?:$)')
 	card_values = []
+	all_cards = []
 
 	for line in inputfile:
 		line = regex.match(line)
@@ -12,7 +13,16 @@ with open(r'./input.txt', mode='r', encoding='utf-8') as inputfile:
 		numbers_in_card = line.group('numbers_in_card').split()
 
 		for hits in [len(list(filter(lambda x: x in numbers_in_card, winning_numbers)))]:
+			all_cards.append([gameid, hits, 1]) # [card id, matches, amount of cards.]
 			if hits: 
 				card_values.append(pow(2,hits-1)) # -1 Because the first hit is worth 1 point
-		
-	print("Answer: {}".format(sum(card_values)))
+
+	# This is really slow but it does the thing.
+	for index, card in enumerate(all_cards):
+		for _ in range(card[2]):
+			i = index + card[1] if index+card[1] < len(all_cards) else len(all_cards)-1
+			while i > index:
+				all_cards[i][2] += 1
+				i -= 1
+			
+	print("Answer 1: {}\nAnswer 2: {}".format(sum(card_values),sum([x[2] for x in all_cards])))
