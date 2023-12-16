5bc5d6e265a51075b1701e7af1c54f12eaadbcba
Arancibia Leiria Leandro Nicanor <Nicanor95@users.noreply.github.com>
2023-12-16 17:32:26 -0300

    Add Day 7 Part 2

--- Day 7/camels.py
+++ Day 7/camels.py
@@ -1,13 +1,22 @@
 from collections import Counter
 
 
-def handValue(hand : str, values : dict) -> int: # This is a mess, likely wont work with all inputs.
+def handValue(hand : str, values : dict, joker = "") -> int: # This is a mess, likely wont work with all inputs.
 	value = 0
+	
+	if joker != "":
+		values[joker] = 1
+
 	for i, card in enumerate(reversed(hand), start = 1):
 		value += (i**100) * values.get(card) 
 	
-	quantities = sorted(Counter(hand).values(), reverse=True)
-	match quantities[0]:
+
+	quantities = Counter(hand)
+	joker_quant = quantities[joker]
+	quantities[joker] = 0
+	
+	quantities = sorted(quantities.values(), reverse=True)
+	match quantities[0] + joker_quant:
 		# Par
 		case 2 if quantities[1] != 2:
 			value **= 2
@@ -30,12 +39,18 @@ def handValue(hand : str, values : dict) -> int: # This is a mess, likely wont w
 	return value
 
 with open(r'./input.txt', mode = 'r', encoding = 'utf-8') as inputfile:
-	values = { "2": 1, "3": 2, "4": 3, "5": 4, "6": 5, "7": 6, "8": 7, "9": 8, "T": 9, "J": 10, "Q": 11, "K": 12, "A": 13 }
+	values = { "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "T": 10, "J": 11, "Q": 12, "K": 13, "A": 14 }
 	hands = [tuple(line.split()) for line in inputfile.readlines()]
 	sorted_hands = sorted(hands, key = lambda x: handValue(x[0], values))
+	sorted_hands_p2 = sorted(hands, key = lambda x: handValue(x[0], values, joker="J"))
 
 	answer = 0
 	for i, hand in enumerate(sorted_hands, start=1):
 		answer += int(hand[1]) * i
 	
+	answer_p2 = 0
+	for i, hand in enumerate(sorted_hands_p2, start=1):
+		answer_p2 += int(hand[1]) * i
+	
 	print("Answer 1: {}".format(answer))
+	print("Answer 2: {}".format(answer_p2))
